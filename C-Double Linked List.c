5b8009755cd32ba96c883f11b50bb25c8ef5a1ef
Anil Sevici <anilsevici@hotmail.com>
2013-04-27 22:08:07 +0300

    C CODES

--- C-Double Linked List.c
+++ C-Double Linked List.c
@@ -3,253 +3,257 @@
 
 struct ogrenci
 {
-    int ogrenci_no;
-    float ogrenci_not;
+    int no;
+    float ort;
     struct ogrenci *onceki;
     struct ogrenci *sonraki;
 };
 
-void d_cift_sirali_ekle(struct ogrenci *liste_basi,struct ogrenci *yeni);
-void artarak_listele(struct ogrenci *liste_basi);
-void azalarak_listele(struct ogrenci *liste_basi);
-int bos_mu(struct ogrenci *liste_basi);
-struct ogrenci *ara(struct ogrenci *liste_basi,int aranan);
-void d_cift_cikar(struct ogrenci *dugum);
-void d_cift_sil(struct ogrenci *dugum);
+int bos_mu(struct ogrenci *liste_basi_ptr);
+void kucukten_buyuge_listele(struct ogrenci *liste_basi_ptr);
+void buyukten_kucuge_listele(struct ogrenci *liste_basi_ptr);
+struct ogrenci *ara(struct ogrenci *liste_basi_ptr, int aranan_no);
+void d_cift_baglidan_dugum_sil(struct ogrenci *silinecek);
+void d_cift_baglidan_dugum_cikar(struct ogrenci *cikarilacak);
+void d_cift_baglidan_dugum_tasi(struct ogrenci *liste_basi,struct ogrenci *dugum);
+void d_cift_sirali_ekle(struct ogrenci *liste_basi_ptr, struct ogrenci *yeni);
 
 int main()
 {
-    struct ogrenci *liste_basi;
-    struct ogrenci *bir_ogrenci;
-    int secim,aranan,sayac;
-    float ogr_not;
-    char cevap;
+    struct ogrenci liste_basi;
+    struct ogrenci *birOgrenci;
+    int no,secim;
+    float min_ort;
+    int sayac;
 
-    liste_basi=malloc(sizeof(struct ogrenci));
-    liste_basi->onceki=liste_basi;
-    liste_basi->sonraki=liste_basi;
+    liste_basi.sonraki=&liste_basi;//liste basi dugumune sahip dairesel cift bagli liste ilkleniyor
+    liste_basi.onceki=&liste_basi;
 
     do
     {
-
-        printf("\n  1.Yeni ogrencinin eklenmesi\n");
-        printf("  2.Tum ogrencilerin ortalamalarina gore kucukten buyuge dogru sirali olarak listelenmesi\n");
-        printf("  3.Tum ogrencilerin ortalamalarina gore buyukten kucuge dogru sirali olarak listelenmesi\n");
-        printf("  4.Numarasi verilen bir ogrencinin not ortalamasinin goruntulenmesi\n");
-        printf("  5.Numarasi verilen bir ogrencinin not ortalamasinin guncellenmesi\n");
-        printf("  6.Numarasi verilen bir ogrencinin silinmesi\n");
-        printf("  7.Not ortalamasi verilen bir sinir degerinin altinda olan ogrencilerin silinmesi\n");
-        printf("  8.Cikis\n");
-
-        printf("\nLutfen Seciminizi Giriniz:");
+        printf("1.Yeni bir ogrenci ekleme\n");
+        printf("2.Ogrencileri not ortalamalarina gore kucukten buyuge dogru listeleme\n");
+        printf("3.Ogrencileri not ortalamalarina gore buyukten kucuge dogru listeleme\n");
+        printf("4.Bir ogrencinin not ortalamasini goruntuleme\n");
+        printf("5.Bir ogrencinin not ortalamasini guncelleme\n");
+        printf("6.Bir ogrenciyi silme\n");
+        printf("7.Ortalamasi dusuk olan ogrencileri silme\n");
+        printf("8.Cikis\n");
+        printf("Seciminizi giriniz:");
         scanf("%d",&secim);
-
-        switch(secim)
+        switch (secim)
         {
         case 1:
-            do
-            {
-                bir_ogrenci=malloc(sizeof(struct ogrenci));
-
-                printf("Ogrencinin nosunu giriniz:");
-                scanf("%d",&bir_ogrenci->ogrenci_no);
-
-                printf("Ogrencinin notunu giriniz:");
-                scanf("%f",&bir_ogrenci->ogrenci_not);
-
-                d_cift_sirali_ekle(liste_basi,bir_ogrenci);
-
-                do
-                {
-                    printf("Yeni ogrenci girecekmisiniz?:");
-                    fflush(stdin);
-                    cevap=getchar();
-                }
-                while(cevap!='E' && cevap!='e' && cevap!='H' && cevap!='h');
-            }
-            while(cevap=='E' || cevap=='e');
+            birOgrenci=malloc(sizeof(struct ogrenci));//yeni ogrenci icin bellekten yer ayriliyor
+            printf("Ogrencinin numarasini giriniz:");
+            scanf("%d",&birOgrenci->no);
+            printf("Ogrencinin not ortalamasini giriniz:");
+            scanf("%f",&birOgrenci->ort);
+            d_cift_sirali_ekle(&liste_basi,birOgrenci);//yeni ogrenci, not ortalamasina gore listenin uygun yerine eklenir
+            printf("Ogrenci eklendi\n");
             break;
         case 2:
-            if(bos_mu(liste_basi))
-                printf("Listelencek kimse yok\n");
+            if (!bos_mu(&liste_basi))
+                kucukten_buyuge_listele(&liste_basi);
             else
-                artarak_listele(liste_basi);
+                printf("Ogrenci listesi bos\n");
             break;
         case 3:
-            if(bos_mu(liste_basi))
-                printf("Listelencek kimse yok\n");
+            if (!bos_mu(&liste_basi))
+                buyukten_kucuge_listele(&liste_basi);
             else
-                azalarak_listele(liste_basi);
+                printf("Ogrenci listesi bos\n");
             break;
         case 4:
-            if(!bos_mu(liste_basi))
+            if (!bos_mu(&liste_basi))
             {
-                printf("Not ortalamasini goruntulemek istediginiz ogrencinin numarasini giriniz:");
-                scanf("%d",&aranan);
-
-                bir_ogrenci=ara(liste_basi,aranan);
-
-                if(bir_ogrenci)
-                    printf("Ogrencinin not ortalamasi:%.2f\n",bir_ogrenci->ogrenci_not);
-                else printf("Boyle bir ogrenci yok.\n");
+                printf("Ogrencinin numarasini giriniz:");
+                scanf("%d",&no);
+                birOgrenci=ara(&liste_basi,no);
+                if (birOgrenci)//ogrenci listede bulunduysa
+                    printf("bu ogrencinin ortalamasi: %.2f\n",birOgrenci->ort);
+                else
+                    printf("bu numarali bir ogrenci yok\n");
             }
-            else printf("Goruntulenecek ogrenci yok.\n");
+            else
+                printf("Ogrenci listesi bos\n");
             break;
         case 5:
-            if(!bos_mu(liste_basi))
+            if (!bos_mu(&liste_basi))
             {
-                printf("Not ortalamasini guncellemek istediginiz ogrencinin numarasini giriniz:");
-                scanf("%d",&aranan);
-
-                bir_ogrenci=ara(liste_basi,aranan);
-
-                if(bir_ogrenci)
+                printf("Ogrencinin numarasini giriniz:");
+                scanf("%d",&no);
+                birOgrenci=ara(&liste_basi,no);
+                if (birOgrenci)//ogrenci listede bulunduysa
                 {
-                    printf("Ogrencinin yeni not ortalamasini giriniz:");
-                    scanf("%f",&bir_ogrenci->ogrenci_not);
+                    printf("bu ogrencinin yeni not ortalamasini giriniz:");
+                    scanf("%f",&birOgrenci->ort);
 
-                    if((bir_ogrenci->onceki!=liste_basi && bir_ogrenci->ogrenci_not<bir_ogrenci->onceki->ogrenci_not) ||
-                            (bir_ogrenci->sonraki!=liste_basi && bir_ogrenci->ogrenci_not>bir_ogrenci->sonraki->ogrenci_not))
+                    if ((birOgrenci->onceki!=&liste_basi && birOgrenci->ort<birOgrenci->onceki->ort) ||
+                            (birOgrenci->sonraki!=&liste_basi && birOgrenci->ort>birOgrenci->sonraki->ort))
                     {
-                        d_cift_cikar(bir_ogrenci);
-                        d_cift_sirali_ekle(liste_basi,bir_ogrenci);
-                    }
-                    else
-                    {
-                        d_cift_cikar(bir_ogrenci);
-                        d_cift_sirali_ekle(liste_basi,bir_ogrenci);
+                        d_cift_baglidan_dugum_tasi(&liste_basi,birOgrenci);
                     }
 
-                    printf("Ogrencinin not ortalamasi basariyla guncellendi.\n");
+                    printf("Ogrencinin not ortalamasi guncellendi\n");
                 }
-                else printf("Boyle bir ogrenci yok.\n");
+                else
+                    printf("bu numarali bir ogrenci yok\n");
             }
-            else printf("Ogrenci listesi bos.\n");
+            else
+                printf("Ogrenci listesi bos\n");
             break;
         case 6:
-            if(!bos_mu(liste_basi))
+            if (!bos_mu(&liste_basi))
             {
-                printf("Silmek istediginiz ogrencinin numarasini giriniz:");
-                scanf("%d",&aranan);
-
-                bir_ogrenci=ara(liste_basi,aranan);
-
-                if(bir_ogrenci)
+                printf("Ogrencinin numarasini giriniz:");
+                scanf("%d",&no);
+                birOgrenci=ara(&liste_basi,no);
+                if (birOgrenci)//ogrenci listede bulunduysa
                 {
-                    d_cift_sil(bir_ogrenci);
-                    printf("Ogrenci basariyla silindi.\n");
+                    d_cift_baglidan_dugum_sil(birOgrenci);//ogrenciyi listeden sil, yani listeden cikar ve yok et
+                    printf("Ogrenci silindi\n");
                 }
-                else printf("Boyle bir ogrenci yok.\n");
+                else
+                    printf("bu numarali bir ogrenci yok\n");
             }
-            else printf("Ogrenci listesi bos.\n");
+            else
+                printf("Ogrenci listesi bos\n");
             break;
         case 7:
-            if(!bos_mu(liste_basi))
+            if (!bos_mu(&liste_basi))
             {
-                printf("Sinir degerin altinda olan ogrencilerin silinmesi icin sinir not giriniz:");
-                scanf("%f",&ogr_not);
-
+                printf("Silinecek ogrencilerin not ortalamasi icin sinir degerini giriniz:");
+                scanf("%f",&min_ort);
                 sayac=0;
-
-                while(liste_basi->sonraki!=liste_basi && liste_basi->sonraki->ogrenci_not<ogr_not)
+                while(liste_basi.sonraki!=&liste_basi && liste_basi.sonraki->ort<min_ort)
                 {
-                    d_cift_sil(liste_basi->sonraki);
+                    d_cift_baglidan_dugum_sil(liste_basi.sonraki);//surekli notu en dusuk olan ogrenciyi sil
                     sayac++;
-
                 }
-                if(sayac>0)
-                    printf("Ogrenciler basariyla silindi.\n");
-                else printf("Ortalamasi sinirin altinda ogrenci yoktur.\n");
+                if (sayac>0)
+                    printf("%d ogrencinin kaydi silindi\n",sayac);
+                else//hic ogrenci silinmediyse
+                    printf("ortalamasi sinirin altinda olan ogrenci yok\n");
             }
-            else printf("Ogrenci listesi bos.\n");
+            else
+                printf("Ogrenci listesi bos\n");
             break;
-
         }
-
     }
-    while(secim!=8);
+    while (secim!=8);
 
     return 0;
 }
 
-void d_cift_sirali_ekle(struct ogrenci *liste_basi,struct ogrenci *yeni)
+void d_cift_sirali_ekle(struct ogrenci *liste_basi_ptr, struct ogrenci *yeni)
 {
     struct ogrenci *gecici;
 
-    gecici=liste_basi->sonraki;
-
-    while(gecici!=liste_basi && gecici->ogrenci_not<yeni->ogrenci_not)
-    {
+    gecici=liste_basi_ptr->sonraki;
+    while (gecici!=liste_basi_ptr && gecici->ort<yeni->ort)
         gecici=gecici->sonraki;
-    }
 
-    yeni->onceki=gecici->onceki;
     yeni->sonraki=gecici;
+    yeni->onceki=gecici->onceki;
     gecici->onceki->sonraki=yeni;
     gecici->onceki=yeni;
 }
 
-int bos_mu(struct ogrenci *liste_basi)
+
+int bos_mu(struct ogrenci *liste_basi_ptr)
 {
-    return liste_basi->sonraki==liste_basi;
+    return liste_basi_ptr->sonraki==liste_basi_ptr;//veya liste_basi_ptr->onceki==liste_basi_ptr
 }
 
-void artarak_listele(struct ogrenci *liste_basi)
+void kucukten_buyuge_listele(struct ogrenci *liste_basi_ptr)
 {
     struct ogrenci *gecici;
 
-    gecici=liste_basi->sonraki;
-    printf("\nNO  NOT\n");
-    printf("--  ---\n");
+    printf("Ogr No Not Ortalamasi\n");
+    printf("------ --------------\n");
 
-
-    while(gecici!=liste_basi)
+    gecici=liste_basi_ptr->sonraki;
+    while (gecici!=liste_basi_ptr)
     {
-        printf(" %d  %-15.2f\n",gecici->ogrenci_no,gecici->ogrenci_not);
+        printf("%-6d %.2f\n",gecici->no,gecici->ort);
         gecici=gecici->sonraki;
     }
 }
 
-void azalarak_listele(struct ogrenci *liste_basi)
+void buyukten_kucuge_listele(struct ogrenci *liste_basi_ptr)
 {
     struct ogrenci *gecici;
 
-    gecici=liste_basi->onceki;
-    printf("\nNO  NOT\n");
-    printf("--  ---\n");
+    printf("Ogr No Not Ortalamasi\n");
+    printf("------ --------------\n");
 
-
-    while(gecici!=liste_basi)
+    gecici=liste_basi_ptr->onceki;
+    while (gecici!=liste_basi_ptr)
     {
-        printf(" %d  %-15.2f\n",gecici->ogrenci_no,gecici->ogrenci_not);
+        printf("%-6d %.2f\n",gecici->no,gecici->ort);
         gecici=gecici->onceki;
     }
 }
 
-struct ogrenci *ara(struct ogrenci *liste_basi,int aranan)
+struct ogrenci *ara(struct ogrenci *liste_basi_ptr, int aranan_no)//ogrenci listede bulunursa isaretcisi dondurulur, bulunamazsa NULL dondurulur
 {
     struct ogrenci *gecici;
 
-    gecici=liste_basi->sonraki;
-
-    while(gecici!=liste_basi)
+    gecici=liste_basi_ptr->sonraki;
+    while (gecici!=liste_basi_ptr)//dikkat:liste not ortalamasina gore sirali, burada numaraya gore arama yapiliyor
     {
-        if (gecici->ogrenci_no==aranan) return gecici;
+        if (gecici->no==aranan_no) return gecici;
         gecici=gecici->sonraki;
     }
 
     return NULL;
 }
 
-void d_cift_cikar(struct ogrenci *dugum)
+void d_cift_baglidan_dugum_tasi(struct ogrenci *liste_basi,struct ogrenci *dugum)//adresi verilen dugumu listeden cikarir ama yok etmez
 {
-    dugum->onceki->sonraki=dugum->sonraki;
-    dugum->sonraki->onceki=dugum->onceki;
+    struct ogrenci *gecici;
+    gecici=dugum;
+
+
+    if(dugum->onceki!=liste_basi && dugum->ort<gecici->onceki->ort)
+    {
+        gecici=gecici->onceki;
+        while(gecici->onceki!=liste_basi && dugum->ort<gecici->onceki->ort)
+            gecici=gecici->onceki;
+
+        dugum->onceki->sonraki=dugum->sonraki;
+        dugum->sonraki->onceki=dugum->onceki;
+
+        dugum->sonraki=gecici;
+        dugum->onceki=gecici->onceki;
+        gecici->onceki->sonraki=dugum;
+        gecici->onceki=dugum;
+    }
+    else
+    {
+        gecici=gecici->sonraki;
+        while(gecici->sonraki!=liste_basi && dugum->ort>gecici->sonraki->ort)
+            gecici=gecici->sonraki;
+
+        dugum->onceki->sonraki=dugum->sonraki;
+        dugum->sonraki->onceki=dugum->onceki;
+
+        dugum->sonraki=gecici->sonraki;
+        dugum->onceki=gecici;
+        gecici->sonraki->onceki=dugum;
+        gecici->sonraki=dugum;
+    }
+
+}
+void d_cift_baglidan_dugum_cikar(struct ogrenci *cikarilacak)//adresi verilen dugumu listeden cikarir ama yok etmez
+{
+    cikarilacak->onceki->sonraki=cikarilacak->sonraki;
+    cikarilacak->sonraki->onceki=cikarilacak->onceki;
 }
 
-void d_cift_sil(struct ogrenci *dugum)
+void d_cift_baglidan_dugum_sil(struct ogrenci *silinecek)//adresi verilen dugumu listeden siler, yani hem listeden cikarir hem yok eder
 {
-    dugum->onceki->sonraki=dugum->sonraki;
-    dugum->sonraki->onceki=dugum->onceki;
-    free(dugum);
+    d_cift_baglidan_dugum_cikar(silinecek);//once dugumu listeden cikar
+    free(silinecek);//sonra dugumu yok et
 }
